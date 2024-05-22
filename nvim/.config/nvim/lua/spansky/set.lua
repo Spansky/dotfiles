1a4c8f10f968f82203bfb6b1e21c03ef7712df73
Leon Sczepansky <leon.sczepansky@boschrexroth.de>
2024-05-22 15:17:44 +0200

    (nvim) have new files open in tabs

--- nvim/.config/nvim/lua/spansky/set.lua
+++ nvim/.config/nvim/lua/spansky/set.lua
@@ -9,4 +9,8 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.api.nvim_set_option("clipboard","unnamed")
-vim.g.netrw_liststyle = 3
+
+-- https://shapeshed.com/vim-netrw/
+vim.g.netrw_liststyle = 3 -- tree view
+vim.g.netrw_banner = 0 -- hide the banner
+vim.g.netrw_browse_split = 3 -- open files in tabs
